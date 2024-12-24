035f249a62c8ca6f03ca86e9ed0d3a4169489620
Alfredo Vidueira <vidueira1992@gmail.com>
2024-12-24 16:42:15 -0300

    chore: README and init script updated

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 Typescript starter project
 
 ```bash
-curl -sSL https://github.com/avidueiram/ts-starter/init.sh | bash
+curl -sSL https://raw.githubusercontent.com/avidueiram/ts-starter/refs/heads/master/init.sh | bash -s -- project-name
 ```
 
--- init.sh
+++ init.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
-echo "Enter the project name:"
-read project_name
-git clone https://github.com/avidueiram/ts-starter.git $project_name
-cd $project_name
+git clone https://github.com/avidueiram/ts-starter.git $1
+cd $1
 rm -rf .git
 git init
 git branch -M master
 git add .
 git commit -m "feat: initial commit"
-echo "Project $project_name initialized in $(pwd)"
+echo "Project $1 initialized in $(pwd)"
